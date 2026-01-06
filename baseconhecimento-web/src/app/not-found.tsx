012fe98d5d369110ef34dc67b31103c807299e81
edivaldosousa <ediribeirosousa@gmail.com>
2026-01-06 03:26:12 +0000

    Add not-found.tsx to diagnose 404 error

---
+++ baseconhecimento-web/src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main>
+      <h1>404 - Página Não Encontrada</h1>
+      <p>A página que você está procurando não existe.</p>
+    </main>
+  );
+}
